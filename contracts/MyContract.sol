a70c27fea11ac78dd762abcd502e91ebfaf5bca2
Hermann EHO <hermannleboss@gmail.com>
2023-03-17 11:28:29 +0000

    feat: inheritance and using only initializing modifier for the parent

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -4,10 +4,19 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract MyContract is Initializable {
+contract BaseContract is Initializable {
+    uint256 public y;
+
+    function initialize() public onlyInitializing {
+        y = 42;
+    }
+}
+
+contract MyContract is BaseContract {
     uint256 public x;
 
     function initialize(uint256 _x) public initializer {
+        BaseContract.initialize(); // Do not forget this call!
         x = _x;
     }
 }
