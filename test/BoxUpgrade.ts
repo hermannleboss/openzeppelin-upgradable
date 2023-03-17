b56e8584ab9ad9a450f86f39f2c63ea9ee7dab4a
Hermann EHO <hermannleboss@gmail.com>
2023-03-17 11:22:40 +0000

    test: Box upgrade testing increment function

--- test/BoxUpgrade.ts
+++ test/BoxUpgrade.ts
@@ -5,20 +5,23 @@ import { expect } from "chai";
 import {ethers, upgrades} from "hardhat";
 
 describe("Box Upgrade", function() {
-  it('Proxy update works', async () => {
+  it('Should create a Box and upgrade it with BoxV2 using proxy', async () => {
     const Box = await ethers.getContractFactory("Box");
     const BoxV2 = await ethers.getContractFactory("BoxV2");
 
     const instance = await upgrades.deployProxy(Box, [42], { initializer: 'store' });
     const upgraded = await upgrades.upgradeProxy(instance.address, BoxV2);
 
-    const value = await upgraded.retrieve();
+    let value = await upgraded.retrieve();
     expect(value.toString()).to.equal('42');
+    await upgraded.increment();
+     value = await upgraded.retrieve();
+    expect(value.toString()).to.equal('43');
   });
 });
 
 describe("Box beacon Upgrade", function() {
-  it('beacon update works', async () => {
+  it('Should create a Box and upgrade it with BoxV2 using beacon', async () => {
     const Box = await ethers.getContractFactory("Box");
     const BoxV2 = await ethers.getContractFactory("BoxV2");
 
@@ -28,7 +31,12 @@ describe("Box beacon Upgrade", function() {
     await upgrades.upgradeBeacon(beacon, BoxV2);
     const upgraded = BoxV2.attach(instance.address);
 
-    const value = await upgraded.retrieve();
+    let value = await upgraded.retrieve();
     expect(value.toString()).to.equal('42');
+
+
+    await upgraded.increment();
+    value = await upgraded.retrieve();
+    expect(value.toString()).to.equal('43');
   });
 });
